21bde949fb603c5e66a0986450aabd121d678aea
Banks Humphreys <iobg0320@gmail.com>
2016-09-28 13:52:52 -0500

    register finished

--- server/controllers/register.js
+++ server/controllers/register.js
@@ -4,21 +4,23 @@ const User = require('../models/user');
 const bcrypt = require('bcrypt')
 
 module.exports.create=(req,res,err)=>{
-	console.log(req.body)
-	if(req.body.password===req.body.confirmaton){
+
+	if(req.body.password===req.body.confirmation){
 	User.findOne({email:req.body.email})
 	.then(user =>{
 		if(user){
 			return res.send({msg: "Email is already registered"})
 		}
-		else return new Promise((resolve,reject)=>{
-			bcrypt.hash(req.body.password,13,(err,hash)=>{
-				if(err) res.send(err)
-				resolve(hash);
+			return new Promise((resolve,reject)=>{
+				bcrypt.hash(req.body.password,13,(err,hash)=>{
+					resolve(hash)
+				})
+			}).then(hash=>{
+				User.create({email:req.body.email, password:hash})
+				res.status(200).send({msg:"User successfully created"})
+			})
 		})
-	}).then(hash=>{
-			return User.create({email:req.body.email,password:hash})
-	})
-	})
+	}
+	
 }
-}
+
