19cf695c78b05bf2680c437fdd09028954d0a252
Marshall Friskics-Warren <wmfwarren@gmail.com>
2016-09-28 15:20:43 -0500

    added redis server functionality

--- server/controllers/register.js
+++ server/controllers/register.js
@@ -4,23 +4,22 @@ const User = require('../models/user');
 const bcrypt = require('bcrypt')
 
 module.exports.create=(req,res,err)=>{
-
-	if(req.body.password===req.body.confirmation){
+	console.log(req.body)
+	if(req.body.password===req.body.confirmaton){
 	User.findOne({email:req.body.email})
 	.then(user =>{
 		if(user){
 			return res.send({msg: "Email is already registered"})
 		}
-			return new Promise((resolve,reject)=>{
-				bcrypt.hash(req.body.password,13,(err,hash)=>{
-					resolve(hash)
-				})
-			}).then(hash=>{
-				User.create({email:req.body.email, password:hash})
-				res.status(200).send({msg:"User successfully created"})
-			})
+		else return new Promise((resolve,reject)=>{
+			bcrypt.hash(req.body.password,13,(err,hash)=>{
+				if(err) res.send(err)
+				resolve(hash);
 		})
-	}
-	
+	}).then(hash=>{
+			return User.create({email:req.body.email,password:hash})
+	})
+	})
 }
-
+	else return res.send({msg: "Passwords do not match"});
+}
