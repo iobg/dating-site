05597fd06a9e9fa730b51ce4e5e9c4c85de1af63
Thomas Swann <tkswann@mac.com>
2016-09-28 14:12:34 -0500

    all routes/partial boilerplate setup

--- client/app/app.js
+++ client/app/app.js
@@ -6,10 +6,38 @@ app.config(['$routeProvider', function($routeProvider) {
   $routeProvider
     .when('/', {
       controller: 'WelcomeCtrl',
+      templateUrl: '/app/partials/welcome.html'
+    })
+    .when('/home', {
+      controller: 'HomeCtrl',
       templateUrl: '/app/partials/home.html'
+    })
+    .when('/login', {
+      controller: 'LoginCtrl',
+      templateUrl: '/app/partials/login.html'
+    })
+    .when('/profile', {
+      controller: 'ProfileCtrl',
+      templateUrl: '/app/partials/profile.html'
     })
 }])
 
 app.controller('WelcomeCtrl', ['$scope', function($scope) {
-  $scope.title = 'Down to Clown'
+  $scope.title = 'Are you down to Clown?'
+  $scope.user = false
+}])
+
+app.controller('HomeCtrl', ['$scope', function($scope) {
+  $scope.title = 'Home page contrller'
+  $scope.user = false
+}])
+
+app.controller('LoginCtrl', ['$scope', function($scope) {
+  $scope.title = 'Login page contrller'
+  $scope.user = true
+}])
+
+app.controller('ProfileCtrl', ['$scope', function($scope) {
+  $scope.title = 'Profile page contrller'
+  $scope.user = true
 }])
