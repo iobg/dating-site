097ef35721ba4c7d066bd9ac7f279d062fd6fdf0
Banks Humphreys <iobg0320@gmail.com>
2016-09-29 12:58:48 -0500

    image hosting finished

--- server/routes/imageupload.js
+++ server/routes/imageupload.js
@@ -11,7 +11,6 @@ const uploadImage = require('../controllers/imageupload')
 imgur.setClientId('9317d730bdae0e9')
 
 router.post('/api/imageupload', upload.single('avatar'), (req,res,next)=>{
-
 	let imagePath = `./images/${req.file.filename}`
 	fs.renameSync(imagePath,`${imagePath}.${req.file.mimetype.slice(6)}`)
 	imgur.uploadFile(`${imagePath}.${req.file.mimetype.slice(6)}`)
