12dad560e60e0a0e99e232c2303a329f129d0d56
Banks Humphreys <iobg0320@gmail.com>
2016-09-29 11:17:23 -0500

    trying to append file extension

--- server/controllers/imageupload.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-const imgur = require('imgur');
-
-imgur.setClientId('9317d730bdae0e9')
-
-module.exports.create=(req,res,next)=>{
-	console.log(req.file)
-}
--- server/routes/imageupload.js
+++ server/routes/imageupload.js
@@ -4,9 +4,20 @@ const { Router }= require('express')
 const router=Router()
 const multer=require('multer')
 const upload=multer({dest:'images/'})
+const imgur = require('imgur')
+const fs = require('fs')
 
 const uploadImage = require('../controllers/imageupload')
+imgur.setClientId('9317d730bdae0e9')
 
-router.post('/api/imageupload', upload.single('avatar'), uploadImage.create)
+router.post('/api/imageupload', upload.single('avatar'), (req,res,next)=>{
+	let imagePath = `../images/${req.file.filename}`
+	fs.renameSync(imagePath,`${imagePath}.${req.file.mimetype.slice(6)}`) 
+	imgur.uploadFile(`../images/${req.file.filename}.${req.file.mimetype.slice(6)}`)
+	.then(json=>{
+		console.log(json.data.link)
+	})
+	.catch(console.error)
+})
 
 module.exports=router;
